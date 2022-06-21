8337082bcb1a32cccde0eeb3130db1deaf5d6385
Marina González Suárez <yelose85@gmail.com>
2022-06-21 19:33:34 +0200

    added stats for

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,15 +1,64 @@
+<script setup>
+    
+        let stats = [
+            {
+                name: "Intelligence",
+                initials: "INT",
+                description: "lorem",
+                image: "src:blablabl",
+                color: "#FFF27D"
+            },
+            {
+                name: "Strength",
+                initials: "STR",
+                description: "lorem",
+                image: "src:blablabl",
+                color: "#DF5F5F"
+            },
+            {
+                name: "Intelligence",
+                initials: "INT",
+                description: "lorem",
+                image: "src:blablabl",
+                color: "#FFF27D"
+            },
+            {
+                name: "Intelligence",
+                initials: "INT",
+                description: "lorem",
+                image: "src:blablabl",
+                color: "#FFF27D"
+            },
+            {
+                name: "Intelligence",
+                initials: "INT",
+                description: "lorem",
+                image: "src:blablabl",
+                color: "#FFF27D"
+            },
+            {
+                name: "Intelligence",
+                initials: "INT",
+                description: "lorem",
+                image: "src:blablabl",
+                color: "#FFF27D"
+            }
+        ]
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div v-for="(item, index) in stats">
+        <section>
+            <p class="initials">{{item.initials}}</p>
+        </section>
+        <section>
+            <span>{{item.name}}:</span>
+            <p>{{item.description}}</p>
+        </section>
+    </div>
+
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+<style scoped>
+
 </style>
+
