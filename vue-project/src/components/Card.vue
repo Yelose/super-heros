567c2a0234a9801503c6243e251dffd6c4ed7a67
Marina González Suárez <yelose85@gmail.com>
2022-07-04 14:08:55 +0200

    created a new component card

---
+++ vue-project/src/components/Card.vue
@@ -0,0 +1,84 @@
+<script>
+import { mapState } from 'pinia';
+import { userCardsFavoritesStore } from '../stores/favorites';
+
+export default {
+    computed: {
+        ...mapState(userCardsFavoritesStore, {
+            favoritesHeroes: 'favoritesHeroes'
+        })
+    },
+}
+</script>
+<template>
+    <div v-for="superHero in favoritesHeroes" class="favorites-item-container"
+        v-bind:style="{ background: superHero.color + '80' }">
+        <div class="card-container" v-bind:style="{ background: superHero.color }">
+            <div class="card-frame">
+                <h4>{{ superHero.name }}</h4>
+                <img class="hero-img-md" v-bind:src="superHero.images.sm" alt="">
+                <div class="card-stats-container">
+                    <section class="stat-bar">
+                        <img class="stat-img" src="/src/assets/img/espada2.png" alt="">
+                        <div class="colorless-bar">
+                            <div class="attack-bar" v-bind:style="{ width: superHero.powerstats.strength + '%' }">
+                            </div>
+                        </div>
+                    </section>
+                    <section class="stat-bar">
+                        <img class="stat-img" src="/src/assets/img/escudo2.png" alt="">
+                        <div class="colorless-bar">
+                            <div class="def-bar" v-bind:style="{ width: superHero.powerstats.durability + '%' }">
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+<style lang="scss">
+.favorites-item-container {
+    width: 100%;
+    border: 3px solid #CFA25B;
+    border-radius: calc(0.3vw + 0.3vh);
+    margin-top: 5vh;
+    padding: 2px;
+
+    .card-container {
+        background: rgb(53, 53, 53);
+        width: 14vw;
+        height: 22vw;
+        border-radius: 4%;
+        max-width: 130px;
+        max-height: 205px;
+
+        .card-frame {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-flow: column nowrap;
+            justify-content: flex-start;
+            align-items: center;
+            background-image: url(../assets/img/marco.PNG);
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            border-radius: 4%;
+
+            h4 {
+                margin-top: 17%;
+                font-size: clamp(8px, 1.5vw, 15px);
+                color: #D9D9D9;
+                font-weight: 100;
+            }
+
+            .hero-img-md {
+                width: 60%;
+                margin-bottom: 4%;
+            }
+        }
+    }
+}
+</style>
--- vue-project/src/components/Favorites.vue
+++ vue-project/src/components/Favorites.vue
@@ -1,12 +1,14 @@
 <script>
 import { mapState } from "pinia";
 import { userCardsFavoritesStore} from "../stores/favorites";
+import Card from "./Card.vue";
 export default {
     computed: {
         ...mapState(userCardsFavoritesStore, {
-            favoritesHeroes: 'favoritesHeroes'
+            favoritesHeroes: "favoritesHeroes"
         })
     },
+    components: { Card }
 }
 </script>
  
@@ -18,30 +20,7 @@ export default {
         <div id="test" v-for="cardsfavorites in Cardsfavorites" :key="key"  style="color:white;">
             {{ cardsfavorites }}
         </div>
-        <div v-for="superHero in favoritesHeroes" class="favorites-item-container" v-bind:style="{ background: superHero.color  + '80'}">
-            <div class="card-container" v-bind:style="{ background: superHero.color}">
-                <div v-on:click="" class="card-frame">
-                    <h4>{{ superHero.name }}</h4>
-                    <img class="hero-img-md" v-bind:src="superHero.images.sm" alt="">
-                    <div class="card-stats-container">
-                        <section class="stat-bar">
-                            <img class="stat-img" src="/src/assets/img/espada2.png" alt="">
-                            <div class="colorless-bar">
-                                <div class="attack-bar" v-bind:style="{ width: superHero.powerstats.strength + '%' }">
-                                </div>
-                            </div>
-                        </section>
-                        <section class="stat-bar">
-                            <img class="stat-img" src="/src/assets/img/escudo2.png" alt="">
-                            <div class="colorless-bar">
-                                <div class="def-bar" v-bind:style="{ width: superHero.powerstats.durability + '%' }">
-                                </div>
-                            </div>
-                        </section>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Card></Card>
     </main>
 </template>
 <style lang="scss">
