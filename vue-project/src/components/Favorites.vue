51539c79d82b5715711d5bf9df14b34edf61a437
Marina González Suárez <yelose85@gmail.com>
2022-07-04 12:15:31 +0200

    copy paste

--- vue-project/src/components/Favorites.vue
+++ vue-project/src/components/Favorites.vue
@@ -1,252 +1,101 @@
 <script>
 import { mapState } from "pinia";
-import { userCardsFavoritesStore } from "../stores/favorites";
+import { userCardsFavoritesStore} from "../stores/favorites";
 export default {
-  computed: {
-    ...mapState(userCardsFavoritesStore, {
-      favoritesHeroes: 'favoritesHeroes'
-    })
-  },
+    computed: {
+        ...mapState(userCardsFavoritesStore, {
+            favoritesHeroes: 'favoritesHeroes'
+        })
+    },
 }
 </script>
-
+ 
 <template>
-  <main class="favorites-container">
-    <button>
-      <h3>Add my personalized card</h3>
-    </button>
-    <div id="test" v-for="cardsfavorites in Cardsfavorites" :key="key" style="color:white;">
-      {{ cardsfavorites }}
-    </div>
-    <div v-for="superHero in favoritesHeroes" class="favorites-item-container"
-      v-bind:style="{ background: superHero.color + '80' }">
-      <div class="card-container" v-bind:style="{ background: superHero.color }">
-        <div v-on:click="" class="card-frame">
-          <h4>{{ superHero.name }}</h4>
-          <img class="hero-img-md" v-bind:src="superHero.images.sm" alt="">
-          <div class="card-stats-container">
-            <section class="stat-bar">
-              <img class="stat-img" src="/src/assets/img/espada2.png" alt="">
-              <div class="colorless-bar">
-                <div class="attack-bar" v-bind:style="{ width: superHero.powerstats.strength + '%' }">
-                </div>
-              </div>
-            </section>
-            <section class="stat-bar">
-              <img class="stat-img" src="/src/assets/img/escudo2.png" alt="">
-              <div class="colorless-bar">
-                <div class="def-bar" v-bind:style="{ width: superHero.powerstats.durability + '%' }">
-                </div>
-              </div>
-            </section>
-          </div>
+    <main class="favorites-container">
+        <button>
+            <h3>Add my personalized card</h3>
+        </button>
+        <div id="test" v-for="cardsfavorites in Cardsfavorites" :key="key"  style="color:white;">
+            {{ cardsfavorites }}
         </div>
-    </div>
-    <section class="stat-bar">
-      <img class="stat-img" src="/src/assets/img/escudo2.png" alt="">
-      <div class="colorless-bar">
-        <div class="def-bar" v-bind:style="{ width: superHero.powerstats.durability + '%' }">
+        <div v-for="superHero in favoritesHeroes" class="favorites-item-container" v-bind:style="{ background: superHero.color  + '80'}">
+            <div class="card-container" v-bind:style="{ background: superHero.color}">
+                <div v-on:click="" class="card-frame">
+                    <h4>{{ superHero.name }}</h4>
+                    <img class="hero-img-md" v-bind:src="superHero.images.sm" alt="">
+                    <div class="card-stats-container">
+                        <section class="stat-bar">
+                            <img class="stat-img" src="/src/assets/img/espada2.png" alt="">
+                            <div class="colorless-bar">
+                                <div class="attack-bar" v-bind:style="{ width: superHero.powerstats.strength + '%' }">
+                                </div>
+                            </div>
+                        </section>
+                        <section class="stat-bar">
+                            <img class="stat-img" src="/src/assets/img/escudo2.png" alt="">
+                            <div class="colorless-bar">
+                                <div class="def-bar" v-bind:style="{ width: superHero.powerstats.durability + '%' }">
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </section>
-    </div>
-    <section class="stat-bar">
-      <img class="stat-img" src="/src/assets/img/escudo2.png" alt="" />
-      <div class="colorless-bar">
-        <div class="def-bar" v-bind:style="{
-          width: superHero.powerstats.durability + '%',
-        }"></div>
-      </div>
-    </section>
-
-
-    <section class="stats-container">
-      <div class="int stat">
-        <span>INT</span>
-        <span>{{ superHero.powerstats.intelligence }}</span>
-      </div>
-      <div class="str stat">
-        <span>STR</span><span>{{ superHero.powerstats.strength }}</span>
-      </div>
-      <div class="spd stat">
-        <span>SPD</span><span>{{ superHero.powerstats.speed }}</span>
-      </div>
-      <div class="dur stat">
-        <span>DUR</span><span>{{ superHero.powerstats.durability }}</span>
-      </div>
-      <div class="pow stat">
-        <span>POW</span><span>{{ superHero.powerstats.power }}</span>
-      </div>
-      <div class="com stat">
-        <span>COM</span><span>{{ superHero.powerstats.combat }}</span>
-      </div>
-    </section>
-
-
-    <section class="card-description-container">
-      <h3 class="full-name">{{ superHero.biography.fullName }}</h3>
-      <article>
-        <p>
-          Group Afiliation: {{ superHero.connections.groupAffiliation }}
-        </p>
-      </article>
-      <article>
-        <p>Relatives: {{ superHero.connections.relatives }}</p>
-      </article>
-      <article>
-        <p>
-          Ocuppation: {{ superHero.work.base }}
-          {{ superHero.work.occupation }}
-        </p>
-      </article>
-    </section>
-    <section class="my-notes">
-      <h3 class="full-name">My Notes</h3>
-    </section>
-
-  </main>
+    </main>
 </template>
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-.full-name {
-  font-family: "Kelly Slab", cursive;
-  color: #fff27d;
-  font-weight: 400;
-  font-family: "Kenia", cursive;
-  -webkit-text-stroke: 1px black;
-}
-
 .favorites-container {
-  display: flex;
-  width: calc(83vw - 150px);
-  padding: 3vw;
-  display: flex;
-  flex-flow: column wrap;
-  align-items: flex-start;
-
-  .cards-and-stats {
-    display: flex;
-  }
-
-  button {
-    background-color: hsla(0, 0%, 0%, 0.6);
-    border-radius: calc(0.3vw + 0.3vh);
-    border-color: #cfa25b;
-    width: 100%;
-    padding: 1vh 1vw;
-  }
-
-  .favorites-item-container {
-    display: flex;
-    flex-flow: row wrap;
-    width: 100%;
-    border: 3px solid #cfa25b;
-    border-radius: calc(0.3vw + 0.3vh);
-    margin-top: 5vh;
-    padding: 2px;
-
-    .text-info-container {
-      display: flex;
-    }
-
-    .card-container {
-      background: rgb(53, 53, 53);
-      width: 14vw;
-      height: 22vw;
-      border-radius: 4%;
-      max-width: 130px;
-      max-height: 205px;
-
-      .card-frame {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        flex-flow: column nowrap;
-        justify-content: flex-start;
-        align-items: center;
-        background-image: url(../assets/img/marco.PNG);
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-position: center;
-        border-radius: 4%;
-
-        h4 {
-          margin-top: 17%;
-          font-size: clamp(8px, 1.5vw, 15px);
-          color: #d9d9d9;
-          font-weight: 100;
-        }
-
-        .hero-img-md {
-          width: 60%;
-          margin-bottom: 4%;
-        }
-      }
-    }
-  }
-
-  .stats-container {
+    width: calc(83vw - 150px);
+    padding: 3vw;
     display: flex;
     flex-flow: column nowrap;
-    justify-content: space-evenly;
-    padding-left: 0.7vw;
-
-    span {
-      justify-content: space-between;
-      padding: 0.3vh 0.5vw;
-      align-items: stretch;
-      font-size: clamp(6px, 1.5vw, 15px);
+    align-items: flex-start;
+    button {
+        background-color: hsla(0, 0%, 0%, 0.6);
+        border-radius: calc(0.3vw + 0.3vh);
+        border-color: #CFA25B;
+        width: 100%;
+        padding: 1vh 1vw;
     }
-  }
-
-  .card-description-container {
-    display: flex;
-    flex-flow: column;
-    align-content: flex-start;
-
-    article {
-      display: flex;
-
-      p {
-        color: white;
-        font-size: clamp(6px, 1.3vw, 13px);
-      }
+    .favorites-item-container {
+        width: 100%;
+        border: 3px solid #CFA25B;
+        border-radius: calc(0.3vw + 0.3vh);
+        margin-top: 5vh;
+        padding: 2px;
+        .card-container {
+            background: rgb(53, 53, 53);
+            width: 14vw;
+            height: 22vw;
+            border-radius: 4%;
+            max-width: 130px;
+            max-height: 205px;
+            .card-frame {
+                width: 100%;
+                height: 100%;
+                display: flex;
+                flex-flow: column nowrap;
+                justify-content: flex-start;
+                align-items: center;
+                background-image: url(../assets/img/marco.PNG);
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-position: center;
+                border-radius: 4%;
+                h4 {
+                    margin-top: 17%;
+                    font-size: clamp(8px, 1.5vw, 15px);
+                    color: #D9D9D9;
+                    font-weight: 100;
+                }
+                .hero-img-md {
+                    width: 60%;
+                    margin-bottom: 4%;
+                }
+            }
+        }
     }
-  }
-}
-
-.stat {
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-}
-
-.int {
-  background-color: #fff27d;
-}
-
-.str {
-  background-color: #df5f5f;
-}
-
-.spd {
-  background-color: #a19740;
-}
-
-.dur {
-  background-color: #7ba4f3;
-}
-
-.pow {
-  background-color: #824084;
-}
-
-.com {
-  background-color: #9ca69d;
 }
 </style>
+
--- vue-project/src/components/Home.vue
+++ vue-project/src/components/Home.vue
@@ -2,137 +2,13 @@
 import { mapActions } from 'pinia'
 import { userCardsFavoritesStore } from '../stores/favorites'
 export default {
-  data() {
-    return {
-      superHeroes: [],
-      superHeroSearch: "",
-      superHeroesShort: [],
-      // audio: "/src/assets/audio/clin.mp3",
-    };
-  },
-  methods: {
-    loadSuperHeroes: async function () {
-      let superHeroesRequest = await fetch(
-        "https://akabab.github.io/superhero-api/api/all.json"
-      );
-      let superHeroes = await superHeroesRequest.json();
-      return superHeroes;
-    },
-    filterSuperHeroes(superHeroes) {
-      return superHeroes.filter(
-        (p) =>
-          p.name.toLowerCase().indexOf(this.superHeroSearch.toLowerCase()) !=
-            -1 && !this.$root.favorites.find((q) => q.id == p.id)
-      );
-    },
-    shortenSuperHeroes(superHeroes) {
-      return superHeroes.splice(0, 40);
-    },
-    searchSuperHeroes() {
-      this.superHeroesShort = this.filterSuperHeroes(this.superHeroes);
-      this.superHeroesShort = this.shortenSuperHeroes(this.superHeroesShort);
-    },
-    selectFavorite(superHero) {
-      this.$root.favorites.push(superHero);
-      this.searchSuperHeroes();
-    },
-    getRaces() {
-      let races = [];
-      let mixedRace = [
-        "Human / Radiation",
-        "Human / Clone",
-        "Human / Cosmic",
-        "Human / Altered",
-        "Human-Kree",
-        "Human-Vuldarian",
-        "Human-Vulcan",
-        "Human-Spartoi",
-        "Mutant / Clone",
-      ];
-      let humanoids = [
-        "Human",
-        "Icthyo Sapien",
-        "Inhuman",
-        "Metahuman",
-        "Amazon",
-        "Mutant",
-        "Tamaranean",
-        "Talokite",
-        "Clone",
-        "Spartoi",
-        "Strontian",
-        "Black Racer",
-        "Alpha",
-      ];
-      let deadOnes = ["Vampire", "Demon", "Parademon", "Zombie", "Korugaran"];
-      let aliens = [
-        "Xenomorph XX121",
-        "Alien",
-        "Martian",
-        "Yautja",
-        "Luphomoid",
-        "Czarnian",
-        "Bolovaxian",
-        "Gungan",
-        "Asgardian",
-        "Rodian",
-        "Zen-Whoberian",
-        "Kakarantharaian",
-        "Kryptonian",
-        "Dathomirian Zabrak",
-      ];
-      let animated = [
-        "Saiyan",
-        "Bizarro",
-        "Neyaphem",
-        "Atlantean",
-        "Symbiote",
-        "Cosmic Entity",
-        "Ungaran",
-      ];
-      let mixed = [
-        "Gorilla",
-        "Animal",
-        "Yoda's species",
-        "Kaiju",
-        "Cyborg",
-        "Android",
-        "Flora Colossus",
-        "Demi-God",
-        "Eternal",
-        "New God",
-        "Frost Giant",
-        "Maiar",
-        "God / Eternal",
-      ];
-      this.superHeroes.forEach((superHero) => {
-        // races.find se considera true, si encuentra una, y false si no.
-        if (!races.find((p) => p == superHero.appearance.race)) {
-          if (superHero.appearance.race == null) {
-            // races.push(superHero.appearance.race)
-            superHero.color = "#001025";
-          }
-          if (mixedRace.indexOf(superHero.appearance.race) != -1) {
-            superHero.color = "#190F00";
-          }
-          if (humanoids.indexOf(superHero.appearance.race) != -1) {
-            superHero.color = "#000000";
-          }
-          if (deadOnes.indexOf(superHero.appearance.race) != -1) {
-            superHero.color = "#500808";
-          }
-          if (aliens.indexOf(superHero.appearance.race) != -1) {
-            superHero.color = "#051900";
-          }
-          if (animated.indexOf(superHero.appearance.race) != -1) {
-            superHero.color = "#000119";
-          }
-          if (mixed.indexOf(superHero.appearance.race) != -1) {
-            superHero.color = "#190016";
-          }
+    data() {
+        return {
+            superHeroes: [],
+            superHeroSearch: '',
+            superHeroesShort: [],
+            // audio: "/src/assets/audio/clin.mp3",
         }
-      });
-      return races;
     },
     methods: {
         loadSuperHeroes: async function () {
@@ -155,7 +31,7 @@ export default {
         },
         ...mapActions(userCardsFavoritesStore, ['addFavorite']),
         selectFavorite(superHero) {
-            this.favoritesHeroes.push(superHero) 
+            this.favoritesHeroes.push(superHero)
             //this.newSuperToAdd = superHero;
             this.addFavorite(superHero)
            
@@ -225,147 +101,118 @@ export default {
         this.superHeroesShort = this.shortenSuperHeroes(this.superHeroesShort)
         this.$root.races = this.getRaces()
     }
-}}
+}
 </script>
-
+ 
 <template>
-  <main>
-    <div class="home-container">
-      <input
-        type="text"
-        placeholder="search"
-        v-on:keyup="searchSuperHeroes()"
-        v-model="superHeroSearch"
-      />
-      <div
-        class="card-container"
-        v-bind:style="{ background: superHero.color }"
-        v-for="superHero in superHeroesShort"
-        v-on:click="selectFavorite(superHero)"
-      >
-        <div v-on:click="" class="card-frame">
-          <h4>{{ superHero.name }}</h4>
-          <img class="hero-img-md" v-bind:src="superHero.images.sm" alt="" />
-          <div class="card-stats-container">
-            <section class="stat-bar">
-              <img class="stat-img" src="/src/assets/img/espada2.png" alt="" />
-              <div class="colorless-bar">
-                <div
-                  class="attack-bar"
-                  v-bind:style="{ width: superHero.powerstats.strength + '%' }"
-                ></div>
-              </div>
-            </section>
-            <section class="stat-bar">
-              <img class="stat-img" src="/src/assets/img/escudo2.png" alt="" />
-              <div class="colorless-bar">
-                <div
-                  class="def-bar"
-                  v-bind:style="{
-                    width: superHero.powerstats.durability + '%',
-                  }"
-                ></div>
-              </div>
-            </section>
-          </div>
+    <main>
+        <div class="home-container">
+            <input type="text" placeholder="search" v-on:keyup="searchSuperHeroes()" v-model="superHeroSearch" />
+            <div class="card-container" v-bind:style="{ background: superHero.color }" v-for="superHero in superHeroesShort"
+                v-on:click="selectFavorite(superHero)">
+                <div v-on:click="" class="card-frame">
+                    <h4>{{ superHero.name }}</h4>
+                    <img class="hero-img-md" v-bind:src="superHero.images.sm" alt="">
+                    <div class="card-stats-container">
+                        <section class="stat-bar">
+                            <img class="stat-img" src="/src/assets/img/espada2.png" alt="">
+                            <div class="colorless-bar">
+                                <div class="attack-bar" v-bind:style="{ width: superHero.powerstats.strength + '%' }"></div>
+                            </div>
+                        </section>
+                        <section class="stat-bar">
+                            <img class="stat-img" src="/src/assets/img/escudo2.png" alt="">
+                            <div class="colorless-bar">
+                                <div class="def-bar" v-bind:style="{ width: superHero.powerstats.durability + '%' }"></div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </main>
-  <aside>
-    <MyCards></MyCards>
-  </aside>
+    </main>
 </template>
-
+ 
 <style lang="scss">
 .home-container {
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-evenly;
-  width: calc(83vw - 150px);
-  padding: 3vw;
-  gap: 1%;
-
-  input {
-    position: fixed;
-    right: 0;
-    top: 6vh;
-    border-radius: 4px;
-    padding: calc(0.5vw + 0.5vh);
-    margin-right: 3vw;
-    width: 16vw;
-    z-index: 102;
-  }
-
-  .card-container {
-    background: rgb(53, 53, 53);
-    width: 14vw;
-    height: 22vw;
-    margin-bottom: 2%;
-    border-radius: 4%;
-    max-width: 130px;
-    max-height: 205px;
-    .card-frame {
-      width: 100%;
-      height: 100%;
-      display: flex;
-      flex-flow: column nowrap;
-      justify-content: flex-start;
-      align-items: center;
-      background-image: url(../assets/img/marco.PNG);
-      background-repeat: no-repeat;
-      background-size: cover;
-      background-position: center;
-      border-radius: 4%;
-
-      h4 {
-        margin-top: 17%;
-        font-size: clamp(8px, 1.5vw, 15px);
-        color: #d9d9d9;
-        font-weight: 100;
-      }
-
-      .hero-img-md {
-        width: 60%;
-        margin-bottom: 4%;
-      }
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+    width: calc(83vw - 150px);
+    padding: 3vw;
+    gap: 1%;
+    input {
+        position: fixed;
+        right: 0;
+        top: 6vh;
+        border-radius: 4px;
+        padding: calc(.5vw + .5vh);
+        margin-right: 3vw;
+        width: 16vw;
+        z-index: 102;
     }
-  }
+    .card-container {
+        background: rgb(53, 53, 53);
+        width: 14vw;
+        height: 22vw;
+        margin-bottom: 2%;
+        border-radius: 4%;
+        max-width: 130px;
+        max-height: 205px;
+        .card-frame {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-flow: column nowrap;
+            justify-content: flex-start;
+            align-items: center;
+            background-image: url(../assets/img/marco.PNG);
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            border-radius: 4%;
+            h4 {
+                margin-top: 17%;
+                font-size: clamp(8px, 1.5vw, 15px);
+                color: #D9D9D9;
+                font-weight: 100;
+            }
+            .hero-img-md {
+                width: 60%;
+                margin-bottom: 4%;
+            }
+        }
+    }
 }
 .card-stats-container {
-  width: 65%;
-  bottom: 0;
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-
-  .stat-bar {
+    width: 65%;
+    bottom: 0;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-bottom: 3%;
-
-    .stat-img {
-      width: 10%;
+    flex-flow: column nowrap;
+    justify-content: center;
+    .stat-bar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding-bottom: 3%;
+        .stat-img {
+            width: 10%;
+        }
+        .attack-bar {
+            background-color: #650f0f;
+            height: 100%;
+        }
+        .colorless-bar {
+            width: 80%;
+            height: 100%;
+            background-color: #C4C4C4;
+            border-radius: 2vw;
+            overflow: hidden;
+        }
+        .def-bar {
+            background-color: rgb(34, 34, 170);
+            height: 100%;
+        }
     }
-
-    .attack-bar {
-      background-color: #650f0f;
-      height: 100%;
-    }
-
-    .colorless-bar {
-      width: 80%;
-      height: 100%;
-      background-color: #c4c4c4;
-      border-radius: 2vw;
-      overflow: hidden;
-    }
-
-    .def-bar {
-      background-color: rgb(34, 34, 170);
-      height: 100%;
-    }
-  }
 }
 </style>
