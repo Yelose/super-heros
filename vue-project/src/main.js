5fdbaf5604d85a0db64bc353a3476f87bd126449
Nerea <fernandezcuestanerea@gmail.com>
2022-07-05 10:40:18 +0200

    changes js

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,11 +4,8 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(VueAwesomePaginate)
 app.mount("#app");
-
-
-
